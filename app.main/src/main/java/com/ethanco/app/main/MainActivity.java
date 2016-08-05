5c24d99921d033896e1b719988c9ec3a06881b72
EthanCo <1406472408@qq.com>
2016-08-05 15:33:37 +0800

    add main bundle

---
+++ app.main/src/main/java/com/ethanco/app/main/MainActivity.java
@@ -0,0 +1,57 @@
+package com.ethanco.app.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import net.wequick.small.Small;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "Z-main";
+    private EditText etUpgrade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        net.wequick.small.Bundle bundle = Small.getBundle("com.ethanco.app.main");
+        File file = bundle.getPatchFile();
+        Log.i(TAG, "onCreate patch path: " + file.getPath());
+
+        etUpgrade = (EditText) findViewById(R.id.et_upgrade);
+
+        findViewById(R.id.btn_upgrade).setOnClickListener(this);
+        findViewById(R.id.btn_go_music_activity).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_upgrade:
+                String bundlePackageName = etUpgrade.getText().toString();
+                if (TextUtils.isEmpty(bundlePackageName)) {
+                    Toast.makeText(this, "请输入Bundle", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                net.wequick.small.Bundle bundle = Small.getBundle(bundlePackageName);
+                Log.i(TAG, "onClick music patch path: " + bundle.getPatchFile().getPath());
+                bundle.upgrade();
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.btn_go_music_activity:
+                Small.openUri("music", MainActivity.this);
+                break;
+            default:
+        }
+    }
+}
