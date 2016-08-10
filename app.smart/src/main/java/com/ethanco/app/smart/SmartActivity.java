512acb96332154f4c1725aa946cac2595315c245
EthanCo <fenghenyu@163.com>
2016-08-10 16:25:45 +0800

    没日懂

---
+++ app.smart/src/main/java/com/ethanco/app/smart/SmartActivity.java
@@ -0,0 +1,13 @@
+package com.ethanco.app.smart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart);
+    }
+}
